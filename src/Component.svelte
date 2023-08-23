2bb98327c4ff849b2de67579250e382262132dc7
Ryan Eaton <scgryan@gmail.com>
2023-08-23 11:28:41 -0400

    versioning

--- src/Component.svelte
+++ src/Component.svelte
@@ -61,7 +61,7 @@
                 0,
                 tableStatuses.splice(dragIndex, 1)[0]
             );
-            refreshColumns(reactiveTableStatuses);
+            refreshColumns(reactiveTableStatuses.detail);
             reactiveTableStatuses = tableStatuses;
         }
         dragIndex = null;
